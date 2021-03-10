aed54695eb2fd55dd42f3d6b67a3ca4e6164ea5c
prpss <1ms19is093@gmail.com>
2021-03-10 17:26:37 +0530

    added basic routing system
    using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return (
+    return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {/* <h4>checking</h4> */}
+          <nav>
+              <li>
+                  <Link to="/">Home</Link>
+              </li>
+              <li>
+                  <Link to="/login">Login</Link>
+                    </li>
+              <li>
+                  <Link to="/reset">Reset</Link>
+              </li>
+          </nav>
+
+       
+            <Switch>
+                 <Route path="/login">
+                    <Login />
+                </Route>
+                 <Route path="/reset">
+                    <Reset />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+     </div>
+    </Router>
+    )
+    
+    function Home() {
+        return <h2>home</h2>;
+    }
+        
+    function Login() {
+        return <h2>login</h2>;
+    }
+        
+    function Reset() {
+        return <h2>reset</h2>;
+    }
 }
 
 export default App;
